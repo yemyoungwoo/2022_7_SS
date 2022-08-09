c090c6b704f8d8f840c72ec30a162c734c4c44c3
yemyoungwoo <tkddlektks@gmail.com>
2022-08-10 02:17:54 +0900

    명령어 입력 연습

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("====시작====");
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("명령어)" );
+		String ss = sc.nextLine();
+//		System.out.printf("입력한 명령어 : %s\n", ss );
+		while (true) {
+			System.out.printf("명령어)" );
+			String cmd = sc.nextLine();
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
+		sc.close();
+		System.out.println("====끝====");
+		
+		
 	}
 }
