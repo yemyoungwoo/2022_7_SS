5a1da639c331d31ec55fa15ca9657278a627ddbb
yemyoungwoo <tkddlektks@gmail.com>
2022-08-11 02:40:35 +0900

    게시물 목록 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,9 @@ public class Main {
 		System.out.println("===시작===");
 
 		Scanner sc = new Scanner(System.in);
+		int 마지막번호 = 0;
+
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.printf("명령어)");
@@ -23,14 +28,30 @@ public class Main {
 			}
 
 			if (cmd.equals("article write")) {
+				int 번호 = 마지막번호 + 1;
+				마지막번호 = 번호;
 				System.out.printf("제목 : ");
 				String ss = sc.nextLine();
 				System.out.printf("내용 : ");
 				String tt = sc.nextLine();
 
-				System.out.printf("1번글이 생성되었습니다. \n");
+				Article article = new Article(마지막번호, ss, tt);
+				articles.add(article);
+
+				System.out.printf("%s번글이 생성되었습니다. \n", 마지막번호);
+
 			} else if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다.");
+					continue;
+
+				}
+				System.out.println("번호    :     제목");
+				for (int i = articles.size() - 1; i >= 0 ; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d      :      %s\n", article.마지막번호 , article.tt);
+				}
+
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
@@ -40,5 +61,18 @@ public class Main {
 		sc.close();
 
 		System.out.println("===끝===");
+	}
+}
+
+class Article {
+	int 마지막번호;
+	String ss;
+	String tt;
+
+	Article(int 마지막번호, String ss, String tt) {
+		this.마지막번호 = 마지막번호;
+		this.tt = tt;
+		this.ss = ss;
+
 	}
 }
